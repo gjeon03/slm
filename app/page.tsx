23cd236b23ff17e9ed16d81bda2e0b88505e6927
gyeongyeon <gjeon0321@gmail.com>
2025-08-10 19:04:32 +0900

    fix: icon 추가

--- app/page.tsx
+++ app/page.tsx
@@ -173,7 +173,15 @@ export default function Page() {
         {/* Header */}
         <header className="flex items-center justify-between bg-white p-4 shadow-sm ">
           <div className="flex items-center gap-2">
-            <div className="w-6 h-6 rounded-md bg-[#F9CE61]" />
+            <div className="w-6 h-6 rounded-md bg-[#F9CE61]">
+              <Image
+                src="/favicon.ico"
+                alt="Logo"
+                width={24}
+                height={24}
+                className="w-full h-full object-cover"
+              />
+            </div>
             <span className="font-semibold tracking-tight">SLM</span>
           </div>
         </header>
